51e380d930c7852a3b45015efe67e3cce0373ccc
Brandon Thomas <brandonvthomas97@gmail.com>
2020-09-04 18:47:36 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,4 @@
 # Build your say_hello method here
-def say_hello(name = "Kent Beck")
+def say_hello(name = "Kent Beck", )
   puts "Hello Kent Beck!"
-end
-def say_hello(name = "Kent Beck")
-   puts "Hello Ruby Programmer!"
 end
